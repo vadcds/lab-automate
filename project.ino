0d22a81be896750ba7b4224caff0e227f5b10834
Neeraj <dvansh896@gmail.com>
2026-04-07 12:41:56 +0530

    Completed smart fan and light automation , PIR, and DHT sensor

--- project.ino
+++ project.ino
@@ -38,7 +38,7 @@ unsigned long interval = 2000;
 unsigned long lastMotionTime = 0;
 int motionDelay = 10000;
 
-// -------- BLYNK --------
+//  BLYNK 
 BLYNK_WRITE(V0) {
   if (!autoMode) {
     fanState = param.asInt() ? LOW : HIGH;
@@ -61,7 +61,7 @@ BLYNK_WRITE(V3) {
   lightAuto = param.asInt();
 }
 
-// -------- SETUP --------
+// SETUP 
 void setup() {
   Serial.begin(115200);
 
@@ -83,4 +83,85 @@ void setup() {
   Blynk.connect();
 }
 
-Added Blynk control and auto/manual mode logic for fan and light
+// LOOP 
+void loop() {
+  Blynk.run();
+
+  //  FAN BUTTON
+  int fanBtn = digitalRead(BUTTON_FAN);
+  if (fanBtn == LOW && lastFanButton == HIGH) {
+    autoMode = false;
+    fanState = !fanState;
+
+    digitalWrite(FAN_RELAY, fanState);
+
+    Blynk.virtualWrite(V0, fanState == LOW ? 1 : 0);
+    Blynk.virtualWrite(V1, 0);
+
+    delay(300);
+  }
+  lastFanButton = fanBtn;
+
+  // LIGHT BUTTON
+  int lightBtn = digitalRead(BUTTON_LIGHT);
+  if (lightBtn == LOW && lastLightButton == HIGH) {
+
+    lightAuto = false;
+
+    lightState = !lightState;
+
+    digitalWrite(LIGHT_RELAY, lightState ? LOW : HIGH);
+    Blynk.virtualWrite(V2, lightState);
+    Blynk.virtualWrite(V3, 0);
+
+    delay(300);
+  }
+  lastLightButton = lightBtn;
+
+  //  PIR 
+  bool motion = digitalRead(PIR_PIN);
+
+  if (motion) {
+    lastMotionTime = millis();
+  }
+
+  bool roomActive = (millis() - lastMotionTime < motionDelay);
+
+ 
+  Blynk.virtualWrite(V6, roomActive);
+
+  // LIGHT AUTO 
+  if (lightAuto) {
+    lightState = roomActive;
+    digitalWrite(LIGHT_RELAY, lightState ? LOW : HIGH);
+  }
+
+  //  SENSOR 
+  if (millis() - lastCheck > interval) {
+    lastCheck = millis();
+
+    float temp = dht.readTemperature();
+    float hum = dht.readHumidity();
+
+    Blynk.virtualWrite(V4, temp);
+    Blynk.virtualWrite(V5, hum);
+
+    //  FAN AUTO 
+    if (autoMode) {
+      if (!roomActive) {
+        fanState = HIGH;
+      }
+      else {
+        if (temp >= 30 || hum >= 70) {
+          fanState = LOW;
+        }
+        else if (temp <= 27 && hum <= 60) {
+          fanState = HIGH;
+        }
+      }
+
+      digitalWrite(FAN_RELAY, fanState);
+      Blynk.virtualWrite(V0, fanState == LOW ? 1 : 0);
+    }
+  }
+}
