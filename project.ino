de5597d52a93fd0cf93cae27eccf2cffa9a2b114
Neeraj <dvansh896@gmail.com>
2026-04-07 11:36:49 +0530

    Added Blynk control and auto/manual mode logic for fan and ligh

--- project.ino
+++ project.ino
@@ -37,3 +37,50 @@ unsigned long interval = 2000;
 // PIR HOLD
 unsigned long lastMotionTime = 0;
 int motionDelay = 10000;
+
+// -------- BLYNK --------
+BLYNK_WRITE(V0) {
+  if (!autoMode) {
+    fanState = param.asInt() ? LOW : HIGH;
+    digitalWrite(FAN_RELAY, fanState);
+  }
+}
+
+BLYNK_WRITE(V1) {
+  autoMode = param.asInt();
+}
+
+BLYNK_WRITE(V2) {
+  if (!lightAuto) {
+    lightState = param.asInt();
+    digitalWrite(LIGHT_RELAY, lightState ? LOW : HIGH);
+  }
+}
+
+BLYNK_WRITE(V3) {
+  lightAuto = param.asInt();
+}
+
+// -------- SETUP --------
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(FAN_RELAY, OUTPUT);
+  pinMode(LIGHT_RELAY, OUTPUT);
+  pinMode(BUTTON_FAN, INPUT_PULLUP);
+  pinMode(BUTTON_LIGHT, INPUT_PULLUP);
+  pinMode(PIR_PIN, INPUT);
+
+  digitalWrite(FAN_RELAY, fanState);
+  digitalWrite(LIGHT_RELAY, HIGH);
+
+  dht.begin();
+
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  Blynk.config(BLYNK_AUTH_TOKEN);
+  Blynk.connect();
+}
+
+Added Blynk control and auto/manual mode logic for fan and light
